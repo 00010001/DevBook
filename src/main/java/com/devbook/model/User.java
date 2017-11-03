630eeaa542325d25808c767aba6f92f8d6d99619
Ola <alexandrastecz@yahoo.pl>
2017-11-03 20:02:45 +0100

    added field List Messages

--- src/main/java/com/devbook/model/User.java
+++ src/main/java/com/devbook/model/User.java
@@ -24,10 +24,60 @@ public class User {
 
    private List<Post> posts;
 
+   private List<Message> messages;
+
 
    public User(String firstName, String lastName, String email) {
       this.firstName = firstName;
       this.lastName = lastName;
       this.email = email;
+   }
+
+   public String getId() {
+      return id;
+   }
+
+   public void setId(String id) {
+      this.id = id;
+   }
+
+   public String getFirstName() {
+      return firstName;
+   }
+
+   public void setFirstName(String firstName) {
+      this.firstName = firstName;
+   }
+
+   public String getLastName() {
+      return lastName;
+   }
+
+   public void setLastName(String lastName) {
+      this.lastName = lastName;
+   }
+
+   public String getEmail() {
+      return email;
+   }
+
+   public void setEmail(String email) {
+      this.email = email;
+   }
+
+   public UserDetails getUserDetails() {
+      return userDetails;
+   }
+
+   public void setUserDetails(UserDetails userDetails) {
+      this.userDetails = userDetails;
+   }
+
+   public List<Post> getPosts() {
+      return posts;
+   }
+
+   public void setPosts(List<Post> posts) {
+      this.posts = posts;
    }
 }

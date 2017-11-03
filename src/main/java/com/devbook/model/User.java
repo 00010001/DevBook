08a11d52d91d02ecb67de54b001ea4b8702b6e03
Grzegorz_B <budnikgrzegorz@interia.pl>
2017-11-03 20:22:16 +0100

    add UserController class add UserService class add annotation to User class

--- src/main/java/com/devbook/model/User.java
+++ src/main/java/com/devbook/model/User.java
@@ -2,6 +2,8 @@ package com.devbook.model;
 
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import org.hibernate.validator.constraints.Email;
 
 import javax.persistence.Id;
 import java.util.List;
@@ -11,23 +13,48 @@ import java.util.List;
 @NoArgsConstructor
 public class User {
 
-   @Id
-   private String id;
+    @Id
+    private String id;
+    @NonNull
+    private String firstName;
+    @NonNull
+    private String lastName;
+    @Email
+    @NonNull
+    private String email;
 
-   private String firstName;
+    private UserDetails userDetails;
 
-   private String lastName;
-
-   private String email;
-
-   private UserDetails userDetails;
-
-   private List<Post> posts;
+    private List<Post> posts;
 
 
-   public User(String firstName, String lastName, String email) {
-      this.firstName = firstName;
-      this.lastName = lastName;
-      this.email = email;
-   }
+    public User(String firstName, String lastName, String email) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setUserDetails(UserDetails userDetails) {
+        this.userDetails = userDetails;
+    }
+
+    public void setPosts(List<Post> posts) {
+        this.posts = posts;
+    }
 }
--- src/main/java/com/devbook/repository/UserRepository.java
+++ src/main/java/com/devbook/repository/UserRepository.java
@@ -3,11 +3,18 @@ package com.devbook.repository;
 import java.util.List;
 
 import com.devbook.model.User;
+import com.devbook.model.UserDetails;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 public interface UserRepository extends MongoRepository<User, String> {
 
-   public User findByFirstName(String firstName);
-   public List<User> findByLastName(String lastName);
+    User findByFirstName(String firstName);
+
+    List<User> findByLastName(String lastName);
+
+    User newUser(String firstName, String lastName, String email);
+
+    UserDetails getUser(String id);
+
 
 }

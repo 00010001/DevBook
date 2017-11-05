26c8e6dedd6bb099b75896d7a5ff00c604cbdebe
Krzysztof Kwiatkowski <reviewalfa1@gmail.com>
2017-11-05 18:25:24 +0100

    cleaned

--- src/main/java/com/devbook/Application.java
+++ src/main/java/com/devbook/Application.java
@@ -1,10 +1,5 @@
 package com.devbook;
 
-import com.devbook.model.User;
-import com.devbook.repository.UserRepository;
-import com.devbook.service.TestService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 

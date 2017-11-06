1d77bdd68303a46f1ec9ba3e4eb699c1537bc8ff
kczecior <kczecior@gmail.com>
2017-11-06 21:23:37 +0100

    zmiania rejestracji

--- src/main/java/com/devbook/service/RegistrationService.java
+++ src/main/java/com/devbook/service/RegistrationService.java
@@ -1,8 +1,11 @@
 package com.devbook.service;
 
+import com.devbook.model.Role;
+import com.devbook.model.User;
 import com.devbook.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.servlet.view.RedirectView;
 
 /**
  * Created by RENT on 2017-11-06.
@@ -17,13 +20,27 @@ public class RegistrationService {
         this.userRepository = userRepository;
     }
 
-    public boolean isEmailTaken(String email){
+    private boolean isEmailTaken(String email){
         if(userRepository.findByEmail(email) != null){
             return true;
         }
         return false;
     }
 
+    public boolean register(String email, String password, String firstName, String lastName) {
+        if(!isEmailTaken(email)) {
+            User user = new User();
+            user.setPassword(password);
+            user.setEmail(email);
+            user.setFirstName(firstName);
+            user.setLastName(lastName);
+            user.getRoleSet().add(Role.ROLE_USER);
+            userRepository.save(user);
+            return true;
+        }
+        else {
+            return false;
+        }
 
-
+    }
 }

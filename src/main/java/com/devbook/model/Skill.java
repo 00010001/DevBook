c2db1e31d311e7c81423996ce5bacd387a49d684
Krzysztof Kwiatkowski <reviewalfa1@gmail.com>
2017-11-07 14:54:51 +0100

    added skill class

---
+++ src/main/java/com/devbook/model/Skill.java
@@ -0,0 +1,13 @@
+package com.devbook.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Skill {
+    private String skillName;
+    private String skillDescription;
+}

623e33df7a7d2267a9f1009c72945589eb885e8b
kczecior <kczecior@gmail.com>
2017-11-08 18:25:42 +0100

    added serach feature + userhomebyid 8.11.2017

---
+++ src/main/java/com/devbook/controller/PostController.java
@@ -0,0 +1,41 @@
+package com.devbook.controller;
+
+import com.devbook.model.Post;
+import com.devbook.service.PostService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
+
+/**
+ * Created by RENT on 2017-11-08.
+ */
+@RestController
+public class PostController {
+
+    PostService postService;
+
+    @Autowired
+    public PostController(PostService postService) {
+        this.postService = postService;
+    }
+
+
+//    @RequestMapping(value = "api/post", method = RequestMethod.POST)
+//    public ModelAndView searchUsers(@RequestParam("exampleFormControlTextarea1") String postBody,
+//                                    Model model){
+//
+//        List<Post> postList = postService.addPost(postBody);
+//        model.addAttribute("postList", postList);
+//
+//        return new ModelAndView("userhome");
+//    }
+
+
+
+}
